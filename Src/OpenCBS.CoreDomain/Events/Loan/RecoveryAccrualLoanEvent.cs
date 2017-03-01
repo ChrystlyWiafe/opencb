c0ae8f5646ee59922c968dd1265cd5fd91baef8a
Dmitry <dmitry.teplov@opencbs.com>
2017-03-01 15:49:14 +0600

    Add 2 new events SALE RALE
    OP-292

---
+++ Src/OpenCBS.CoreDomain/Events/Loan/RecoveryAccrualLoanEvent.cs
@@ -0,0 +1,17 @@
+﻿namespace OpenCBS.CoreDomain.Events.Loan
+{
+    public class RecoveryAccrualLoanEvent : Event
+    {
+        public override string Code
+        {
+            get { return "RALE"; }
+            set { _code = value; }
+        }
+        public override string Description { get; set; }
+
+        public override Event Copy()
+        {
+            return (RecoveryAccrualLoanEvent)MemberwiseClone();
+        }
+    }
+}
---
+++ Src/OpenCBS.CoreDomain/Events/Loan/StopAccrualLoanEvent.cs
@@ -0,0 +1,17 @@
+﻿namespace OpenCBS.CoreDomain.Events.Loan
+{
+    public class StopAccrualLoanEvent : Event
+    {
+        public override string Code
+        {
+            get { return "SALE"; }
+            set { _code = value; }
+        }
+        public override string Description { get; set; }
+
+        public override Event Copy()
+        {
+            return (StopAccrualLoanEvent)MemberwiseClone();
+        }
+    }
+}
