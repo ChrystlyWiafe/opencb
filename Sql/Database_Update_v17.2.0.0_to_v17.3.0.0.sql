2f77d95723e27bd3c198d3bb9f9d1a8c3289f964
Osminin Sergey <souloffiree@gmail.com>
2017-03-03 14:22:04 +0600

    Fix incorrect names of the actions OP-294

--- Sql/Database_Update_v17.2.0.0_to_v17.3.0.0.sql
+++ Sql/Database_Update_v17.2.0.0_to_v17.3.0.0.sql
@@ -3,9 +3,9 @@ SET     [value] = 'v17.3.0.0'
 WHERE   [name] = 'VERSION'
 GO
 
-IF (SELECT TOP 1 [method_name] FROM dbo.[ActionItems] WHERE [method_name] = 'StopPenaltyForLoan') IS NULL
+IF (SELECT TOP 1 [method_name] FROM dbo.[ActionItems] WHERE [method_name] = 'StopPenalty') IS NULL
     BEGIN				
-		INSERT INTO [dbo].[ActionItems]([class_name], [method_name]) VALUES ('LoanServices', 'StopPenaltyForLoan')
+		INSERT INTO [dbo].[ActionItems]([class_name], [method_name]) VALUES ('LoanServices', 'StopPenalty')
 		DECLARE @id INT = SCOPE_IDENTITY()
 		INSERT INTO [dbo].[AllowedRoleActions](action_item_id,role_id,allowed)
 		SELECT
@@ -19,26 +19,9 @@ IF (SELECT TOP 1 [method_name] FROM dbo.[ActionItems] WHERE [method_name] = 'Sto
 		FROM dbo.Roles r
     END
 GO
-IF (SELECT TOP 1 [method_name] FROM dbo.[ActionItems] WHERE [method_name] = 'StopInterestForLoan') IS NULL
+IF (SELECT TOP 1 [method_name] FROM dbo.[ActionItems] WHERE [method_name] = 'StopInterest') IS NULL
     BEGIN				
-		INSERT INTO [dbo].[ActionItems]([class_name], [method_name]) VALUES ('LoanServices', 'StopInterestForLoan')
-		DECLARE @id INT = SCOPE_IDENTITY()
-		INSERT INTO [dbo].[AllowedRoleActions](action_item_id,role_id,allowed)
-		SELECT
-			@id
-			, r.id
-			, CASE
-					WHEN r.code in ('ADMIN','SUPER')
-					THEN 1
-					ELSE 0
-				END
-		FROM dbo.Roles r
-    END
-GO
-
-IF (SELECT TOP 1 [method_name] FROM dbo.[ActionItems] WHERE [method_name] = 'RecoverPenaltyForLoan') IS NULL
-    BEGIN				
-		INSERT INTO [dbo].[ActionItems]([class_name], [method_name]) VALUES ('LoanServices', 'RecoverPenaltyForLoan')
+		INSERT INTO [dbo].[ActionItems]([class_name], [method_name]) VALUES ('LoanServices', 'StopInterest')
 		DECLARE @id INT = SCOPE_IDENTITY()
 		INSERT INTO [dbo].[AllowedRoleActions](action_item_id,role_id,allowed)
 		SELECT
@@ -53,9 +36,26 @@ IF (SELECT TOP 1 [method_name] FROM dbo.[ActionItems] WHERE [method_name] = 'Rec
     END
 GO
 
-IF (SELECT TOP 1 [method_name] FROM dbo.[ActionItems] WHERE [method_name] = 'RecoverInterestForLoan') IS NULL
+IF (SELECT TOP 1 [method_name] FROM dbo.[ActionItems] WHERE [method_name] = 'RecoverPenalty') IS NULL
     BEGIN				
-		INSERT INTO [dbo].[ActionItems]([class_name], [method_name]) VALUES ('LoanServices', 'RecoverInterestForLoan')
+		INSERT INTO [dbo].[ActionItems]([class_name], [method_name]) VALUES ('LoanServices', 'RecoverPenalty')
+		DECLARE @id INT = SCOPE_IDENTITY()
+		INSERT INTO [dbo].[AllowedRoleActions](action_item_id,role_id,allowed)
+		SELECT
+			@id
+			, r.id
+			, CASE
+					WHEN r.code in ('ADMIN','SUPER')
+					THEN 1
+					ELSE 0
+				END
+		FROM dbo.Roles r
+    END
+GO
+
+IF (SELECT TOP 1 [method_name] FROM dbo.[ActionItems] WHERE [method_name] = 'RecoverInterest') IS NULL
+    BEGIN				
+		INSERT INTO [dbo].[ActionItems]([class_name], [method_name]) VALUES ('LoanServices', 'RecoverInterest')
 		DECLARE @id INT = SCOPE_IDENTITY()
 		INSERT INTO [dbo].[AllowedRoleActions](action_item_id,role_id,allowed)
 		SELECT
