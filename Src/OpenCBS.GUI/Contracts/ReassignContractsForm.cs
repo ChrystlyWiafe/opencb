56243ca00814b4ce42ea0adf83424a1feca30919
Osminin Sergey <souloffiree@gmail.com>
2017-04-11 15:45:18 +0600

    Undo changes from commit 2a31534bb08

--- Src/OpenCBS.GUI/Contracts/ReassignContractsForm.cs
+++ Src/OpenCBS.GUI/Contracts/ReassignContractsForm.cs
@@ -94,7 +94,6 @@ namespace OpenCBS.GUI.Contracts
             _users = ServicesProvider.GetInstance()
                                      .GetUserServices()
                                      .FindAll(_incudeDeletedUsersCheckBox.Checked)
-                                     .Where(item => item.UserRole.RoleName == "LOF")
                                      .OrderBy(item => item.LastName)
                                      .ThenBy(item => item.FirstName)
                                      .ToList();
