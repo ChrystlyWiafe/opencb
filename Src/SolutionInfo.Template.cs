16a5dac9be73dc7e7deb937e415305d55f2cfa6d
Osminin Sergey <souloffiree@gmail.com>
2017-05-04 11:04:40 +0600

    Bump to version 17.4 OP-339

--- Src/SolutionInfo.Template.cs
+++ Src/SolutionInfo.Template.cs
@@ -22,7 +22,7 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("Open Octopus Ltd.")]
 [assembly: AssemblyProduct("OPENCBS")]
-[assembly: AssemblyVersion("17.3.build.0")]
+[assembly: AssemblyVersion("17.4.build.0")]
 [assembly: AssemblyGitRevision("revision")]
 
 [AttributeUsage(AttributeTargets.Assembly)]
