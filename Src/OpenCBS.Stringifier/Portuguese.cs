a53a71594c61b2cb08607c5d3bb371934d75e36e
baktiyar <dushebaev.baktiyar@gmail.com>
2017-06-09 11:41:17 +0600

    Add Stringifier for Portuguese

--- Src/OpenCBS.Stringifier/Portuguese.cs
+++ Src/OpenCBS.Stringifier/Portuguese.cs
@@ -7,7 +7,6 @@ namespace OpenCBS.Stringifier
 {
     public class Portuguese: IStringifiable
     {
-        // O método toExtenso recebe um valor do tipo decimal
         public string Stringify(decimal valor)
         {
             if (valor <= 0 | valor >= 1000000000000000)
@@ -19,7 +18,97 @@ namespace OpenCBS.Stringifier
 
                 for (int i = 0; i <= 15; i += 3)
                 {
-                    valor_por_extenso += Stringify(Convert.ToDecimal(strValor.Substring(i, 3)));
+                    valor_por_extenso += innerStringify(Convert.ToDecimal(strValor.Substring(i, 3)));
+                    if (i == 0 & valor_por_extenso != string.Empty)
+                    {
+                        if (Convert.ToInt32(strValor.Substring(0, 3)) == 1)
+                            valor_por_extenso += " TRILHÃO" +
+                                                 ((Convert.ToDecimal(strValor.Substring(3, 12)) > 0)
+                                                     ? " E "
+                                                     : string.Empty);
+                        else if (Convert.ToInt32(strValor.Substring(0, 3)) > 1)
+                            valor_por_extenso += " TRILHÕES" +
+                                                 ((Convert.ToDecimal(strValor.Substring(3, 12)) > 0)
+                                                     ? " E "
+                                                     : string.Empty);
+                    }
+                    else if (i == 3 & valor_por_extenso != string.Empty)
+                    {
+                        if (Convert.ToInt32(strValor.Substring(3, 3)) == 1)
+                            valor_por_extenso += " BILHÃO" +
+                                                 ((Convert.ToDecimal(strValor.Substring(6, 9)) > 0)
+                                                     ? " E "
+                                                     : string.Empty);
+                        else if (Convert.ToInt32(strValor.Substring(3, 3)) > 1)
+                            valor_por_extenso += " BILHÕES" +
+                                                 ((Convert.ToDecimal(strValor.Substring(6, 9)) > 0)
+                                                     ? " E "
+                                                     : string.Empty);
+                    }
+                    else if (i == 6 & valor_por_extenso != string.Empty)
+                    {
+                        if (Convert.ToInt32(strValor.Substring(6, 3)) == 1)
+                            valor_por_extenso += " MILHÃO" +
+                                                 ((Convert.ToDecimal(strValor.Substring(9, 6)) > 0)
+                                                     ? " E "
+                                                     : string.Empty);
+                        else if (Convert.ToInt32(strValor.Substring(6, 3)) > 1)
+                            valor_por_extenso += " MILHÕES" +
+                                                 ((Convert.ToDecimal(strValor.Substring(9, 6)) > 0)
+                                                     ? " E "
+                                                     : string.Empty);
+                    }
+                    else if (i == 9 & valor_por_extenso != string.Empty)
+                        if (Convert.ToInt32(strValor.Substring(9, 3)) > 0)
+                            valor_por_extenso += " MIL" +
+                                                 ((Convert.ToDecimal(strValor.Substring(12, 3)) > 0)
+                                                     ? " E "
+                                                     : string.Empty);
+
+                    if (i == 12)
+                    {
+                        if (valor_por_extenso.Length > 8)
+                            if (valor_por_extenso.Substring(valor_por_extenso.Length - 6, 6) == "BILHÃO" |
+                                valor_por_extenso.Substring(valor_por_extenso.Length - 6, 6) == "MILHÃO")
+                                valor_por_extenso += " DE";
+                            else if (valor_por_extenso.Substring(valor_por_extenso.Length - 7, 7) == "BILHÕES" |
+                                     valor_por_extenso.Substring(valor_por_extenso.Length - 7, 7) == "MILHÕES" |
+                                     valor_por_extenso.Substring(valor_por_extenso.Length - 8, 7) == "TRILHÕES")
+                                valor_por_extenso += " DE";
+                            else if (valor_por_extenso.Substring(valor_por_extenso.Length - 8, 8) == "TRILHÕES")
+                                valor_por_extenso += " DE";
+
+                        if (Convert.ToInt64(strValor.Substring(0, 15)) == 1)
+                            valor_por_extenso += " KWANZA";
+                        else if (Convert.ToInt64(strValor.Substring(0, 15)) > 1)
+                            valor_por_extenso += " KWANZAS";
+
+                        if (Convert.ToInt32(strValor.Substring(16, 2)) > 0 && valor_por_extenso != string.Empty)
+                            valor_por_extenso += " E ";
+                    }
+
+                    if (i == 15)
+                        if (Convert.ToInt32(strValor.Substring(16, 2)) == 1)
+                            valor_por_extenso += " CENTIMO";
+                        else if (Convert.ToInt32(strValor.Substring(16, 2)) > 1)
+                            valor_por_extenso += " CENTIMOS";
+                }
+                return valor_por_extenso;
+            }
+        }
+        // O método toExtenso recebe um valor do tipo decimal
+        public string StringifyWithPercent(decimal valor)
+        {
+            if (valor <= 0 | valor >= 1000000000000000)
+                return "Valor não suportado pelo sistema.";
+            else
+            {
+                string strValor = valor.ToString("000000000000000.00");
+                string valor_por_extenso = string.Empty;
+
+                for (int i = 0; i <= 15; i += 3)
+                {
+                    valor_por_extenso += innerStringify(Convert.ToDecimal(strValor.Substring(i, 3)));
                     if (i == 0 & valor_por_extenso != string.Empty)
                     {
                         if (Convert.ToInt32(strValor.Substring(0, 3)) == 1)
@@ -98,7 +187,7 @@ namespace OpenCBS.Stringifier
             }
         }
 
-        public string StringifyWithPercent(decimal valor)
+        private string innerStringify(decimal valor)
         {
             if (valor <= 0)
                 return string.Empty;
