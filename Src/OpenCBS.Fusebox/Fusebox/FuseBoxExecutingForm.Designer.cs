10621c8265938c68b02758a2eadb1b1f702bdeb4
Osminin Sergey <souloffiree@gmail.com>
2017-06-23 14:13:23 +0600

    Add localization to the fusebox form OP-388

--- Src/OpenCBS.Fusebox/Fusebox/FuseBoxExecutingForm.Designer.cs
+++ Src/OpenCBS.Fusebox/Fusebox/FuseBoxExecutingForm.Designer.cs
@@ -39,60 +39,42 @@
             // 
             // progressBarLoans
             // 
-            this.progressBarLoans.Location = new System.Drawing.Point(59, 120);
+            resources.ApplyResources(this.progressBarLoans, "progressBarLoans");
             this.progressBarLoans.Name = "progressBarLoans";
-            this.progressBarLoans.Size = new System.Drawing.Size(310, 27);
-            this.progressBarLoans.TabIndex = 0;
             // 
             // progressBarFuses
             // 
-            this.progressBarFuses.Location = new System.Drawing.Point(59, 49);
+            resources.ApplyResources(this.progressBarFuses, "progressBarFuses");
             this.progressBarFuses.Name = "progressBarFuses";
-            this.progressBarFuses.Size = new System.Drawing.Size(310, 27);
-            this.progressBarFuses.TabIndex = 0;
             // 
             // btnStop
             // 
-            this.btnStop.Location = new System.Drawing.Point(248, 168);
+            resources.ApplyResources(this.btnStop, "btnStop");
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(99, 26);
-            this.btnStop.TabIndex = 1;
-            this.btnStop.Text = "Stop";
             this.btnStop.UseVisualStyleBackColor = true;
             this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(88, 168);
+            resources.ApplyResources(this.btnStart, "btnStart");
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(99, 26);
-            this.btnStart.TabIndex = 1;
-            this.btnStart.Text = "Start";
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(56, 31);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(33, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Fuse:";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(85, 31);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 13);
-            this.label2.TabIndex = 2;
             // 
             // FuseBoxExecutingForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(439, 210);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnStart);
@@ -100,12 +82,9 @@
             this.Controls.Add(this.progressBarFuses);
             this.Controls.Add(this.progressBarLoans);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FuseBoxExecutingForm";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Day Closure";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FuseBoxExecutingForm_FormClosing);
             this.Load += new System.EventHandler(this.FuseBoxExecutingForm_Load);
             this.ResumeLayout(false);
