dab90f02537d389efd16c08bf1856092e1d81ade
baktiyar <dushebaev.baktiyar@gmail.com>
2017-07-20 12:01:36 +0600

    Add index for tables "LoanDisbursmentEvents", "LoanEntryFeeEvents" and "Contracts"

--- Sql/Database_Update_v17.6.0.0_to_v17.7.0.0.sql
+++ Sql/Database_Update_v17.6.0.0_to_v17.7.0.0.sql
@@ -2,3 +2,21 @@ UPDATE  [TechnicalParameters]
 SET     [value] = 'v17.7.0.0'
 WHERE   [name] = 'VERSION'
 GO
+  
+IF NOT EXISTS(SELECT * FROM sys.indexes WHERE Name = 'IX_LoanDisbursmentEvents_amount_fees_payment_method_id')
+CREATE NONCLUSTERED INDEX [IX_LoanDisbursmentEvents_amount_fees_payment_method_id]
+ON [dbo].[LoanDisbursmentEvents] ([id])
+INCLUDE ([amount],[fees],[payment_method_id])
+GO
+
+IF NOT EXISTS(SELECT * FROM sys.indexes WHERE Name = 'IX_LoanEntryFeeEvents_fees_disbursement_event_id')
+CREATE NONCLUSTERED INDEX [IX_LoanEntryFeeEvents_fees_disbursement_event_id]
+ON [dbo].[LoanEntryFeeEvents] ([id])
+INCLUDE ([fee],[disbursement_event_id])
+GO
+
+IF NOT EXISTS(SELECT * FROM sys.indexes WHERE Name = 'IX_Contracts_status')
+CREATE NONCLUSTERED INDEX [IX_Contracts_status]
+ON [dbo].[Contracts] ([project_id],[nsg_id])
+INCLUDE ([status])
+GO
